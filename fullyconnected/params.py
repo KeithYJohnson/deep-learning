677ed7b299881c1208bddbf118dbd554c89dc0aa
Keith Johnson <keithyjohnson@gmail.com>
2016-12-09 16:22:08 -0500

    Setting up hyper params and dependencies for script

---
+++ fullyconnected/params.py
@@ -0,0 +1,6 @@
+data_dir =    '/Users/keithjohnson/datasets'
+pickle_file = '/Users/keithjohnson/datasets/notMNIST.pickle'
+train_subset = 10000
+image_size = 28
+num_labels = 10
+num_steps = 801
---
+++ fullyconnected/run.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+import numpy as np
+import tensorflow as tf
+from six.moves import cPickle as pickle
+from six.moves import range
+from params import *
