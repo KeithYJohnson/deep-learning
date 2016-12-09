977ff0edff9063ba8d4e84806130a62c23fb2eea
Keith Johnson <keithyjohnson@gmail.com>
2016-12-09 14:38:50 -0500

    gotta shuffle the data ya know?

---
+++ notmnist/shuffle_dataset.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def shuffle_dataset(dataset, labels):
+    permutation = np.random.permutation(labels.shape[0])
+    shuffled_dataset = dataset[permutation,:,:]
+    shuffled_labels = labels[permutation]
+    return shuffled_dataset, shuffled_labels
