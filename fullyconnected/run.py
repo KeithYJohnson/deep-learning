7ded88b2808df66b14aa720d00c99d024e74c45a
Keith Johnson <keithyjohnson@gmail.com>
2016-12-09 16:22:53 -0500

    Reformatting the data

--- fullyconnected/run.py
+++ fullyconnected/run.py
@@ -5,9 +5,14 @@ from six.moves import cPickle as pickle
 from six.moves import range
 from params import *
 from load_data import *
+from reformat import *
 [train_dataset,
  train_labels,
  valid_dataset,
  valid_labels,
  test_dataset,
  test_labels] = load_data(pickle_file)
+
+train_dataset, train_labels = reformat(train_dataset, train_labels)
+valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
+test_dataset, test_labels = reformat(test_dataset, test_labels)
