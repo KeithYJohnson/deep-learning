50c4c32f60658b941cca72d6cd9aa66fb0024aee
Keith Johnson <keithyjohnson@gmail.com>
2016-12-09 16:23:53 -0500

    Setting up and running computation on the graph.

--- fullyconnected/run.py
+++ fullyconnected/run.py
@@ -6,6 +6,8 @@ from six.moves import range
 from params import *
 from load_data import *
 from reformat import *
+from set_up_graph_and_run import *
+
 [train_dataset,
  train_labels,
  valid_dataset,
@@ -19,3 +21,6 @@ test_dataset, test_labels = reformat(test_dataset, test_labels)
 print('Training set', train_dataset.shape, train_labels.shape)
 print('Validation set', valid_dataset.shape, valid_labels.shape)
 print('Test set', test_dataset.shape, test_labels.shape)
+
+graph = tf.Graph()
+set_up_graph_and_run(graph, train_dataset, train_labels, valid_dataset, valid_labels, test_dataset, test_labels)
