c7940da385bec82d94626d2953cce8824b1e8246
Keith Johnson <keithyjohnson@gmail.com>
2016-12-09 16:22:25 -0500

    Function to load the data

--- fullyconnected/run.py
+++ fullyconnected/run.py
@@ -4,3 +4,10 @@ import tensorflow as tf
 from six.moves import cPickle as pickle
 from six.moves import range
 from params import *
+from load_data import *
+[train_dataset,
+ train_labels,
+ valid_dataset,
+ valid_labels,
+ test_dataset,
+ test_labels] = load_data(pickle_file)
