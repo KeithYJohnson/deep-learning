57fe118e897f6bf1673fef923eda8e1a5804e6e2
Keith Johnson <keithyjohnson@gmail.com>
2016-12-09 18:28:41 -0500

    just sprucing things up a bit

--- fullyconnected/run.py
+++ fullyconnected/run.py
@@ -15,12 +15,20 @@ from set_up_graph_and_run import *
  test_dataset,
  test_labels] = load_data(pickle_file)
 
-train_dataset, train_labels = reformat(train_dataset, train_labels)
-valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
-test_dataset, test_labels = reformat(test_dataset, test_labels)
-print('Training set', train_dataset.shape, train_labels.shape)
-print('Validation set', valid_dataset.shape, valid_labels.shape)
-print('Test set', test_dataset.shape, test_labels.shape)
+reformatted_train_dataset, reformatted_train_labels = reformat(train_dataset, train_labels)
+reformatted_valid_dataset, reformatted_valid_labels = reformat(valid_dataset, valid_labels)
+reformatted_test_dataset, reformatted_test_labels = reformat(test_dataset, test_labels)
+print('reformatted Training set', reformatted_train_dataset.shape, reformatted_train_labels.shape)
+print('reformatted Validation set', reformatted_valid_dataset.shape, reformatted_valid_labels.shape)
+print('reformatted Test set', reformatted_test_dataset.shape, reformatted_test_labels.shape)
 
 graph = tf.Graph()
-set_up_graph_and_run(graph, train_dataset, train_labels, valid_dataset, valid_labels, test_dataset, test_labels)
+set_up_graph_and_run(
+    graph,
+    reformatted_train_dataset,
+    reformatted_train_labels,
+    reformatted_valid_dataset,
+    reformatted_valid_labels,
+    reformatted_test_dataset,
+    reformatted_test_labels
+)
