7e2e61e21c2ac433c37c2dfa678b1cd5417d17ab
Keith Johnson <keithyjohnson@gmail.com>
2016-12-13 15:49:19 -0500

    reading the data

--- word2vec/run.py
+++ word2vec/run.py
@@ -6,4 +6,7 @@ import tensorflow as tf
 from matplotlib import pylab
 from six.moves import range
 from sklearn.manifold import TSNE
+from read_data import *
 filename = 'text8.zip'
+words = read_data(filename)
+print('Data size %d' % len(words))
