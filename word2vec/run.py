b1ce2633b931af450946176a6c426e6c24f6247e
Keith Johnson <keithyjohnson@gmail.com>
2016-12-13 15:49:51 -0500

    Adding function to build the dataset

--- word2vec/run.py
+++ word2vec/run.py
@@ -7,6 +7,12 @@ from matplotlib import pylab
 from six.moves import range
 from sklearn.manifold import TSNE
 from read_data import *
+from build_dataset import *
 filename = 'text8.zip'
 words = read_data(filename)
 print('Data size %d' % len(words))
+
+data, count, dictionary, reverse_dictionary = build_dataset(words)
+print('Most common words (+UNK)', count[:5])
+print('Sample data', data[:10])
+del words  # Hint to reduce memory.
