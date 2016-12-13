7cd3bcd79d91fdf22ef874ca197717f591bbf331
Keith Johnson <keithyjohnson@gmail.com>
2016-12-13 15:50:45 -0500

    Generating batches.

--- word2vec/run.py
+++ word2vec/run.py
@@ -8,6 +8,7 @@ from six.moves import range
 from sklearn.manifold import TSNE
 from read_data import *
 from build_dataset import *
+from generate_batch import *
 filename = 'text8.zip'
 words = read_data(filename)
 print('Data size %d' % len(words))
@@ -16,3 +17,10 @@ data, count, dictionary, reverse_dictionary = build_dataset(words)
 print('Most common words (+UNK)', count[:5])
 print('Sample data', data[:10])
 del words  # Hint to reduce memory.
+
+for num_skips, skip_window in [(2, 1), (4, 2)]:
+    data_index = 0
+    batch, labels = generate_batch(data, batch_size=8, num_skips=num_skips, skip_window=skip_window)
+    print('\nwith num_skips = %d and skip_window = %d:' % (num_skips, skip_window))
+    print('    batch:', [reverse_dictionary[bi] for bi in batch])
+    print('    labels:', [reverse_dictionary[li] for li in labels.reshape(8)])
