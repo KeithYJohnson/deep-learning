8ead78b82d56ba5229d0cf23cd7e16f2ef5d0a56
Keith Johnson <keithyjohnson@gmail.com>
2016-12-13 11:52:37 -0500

    Commenting in the reformatted cube shapes so I dont have to run the net to get them.

--- convolutions/run.py
+++ convolutions/run.py
@@ -20,9 +20,13 @@ from run_conv_net import *
 train_dataset, train_labels = reformat_as_cube(train_dataset, train_labels)
 valid_dataset, valid_labels = reformat_as_cube(valid_dataset, valid_labels)
 test_dataset, test_labels = reformat_as_cube(test_dataset, test_labels)
+
 print('Cube Reformatted Training set', train_dataset.shape, train_labels.shape)
+# Cube Reformatted Training set (200000, 28, 28, 1) (200000, 10)
 print('Cube Reformatted Validation set', valid_dataset.shape, valid_labels.shape)
+# Cube Reformatted Validation set (10000, 28, 28, 1) (10000, 10)
 print('Cube Reformatted Test set', test_dataset.shape, test_labels.shape)
+# Cube Reformatted Test set (10000, 28, 28, 1) (10000, 10)
 
 graph = tf.Graph()
 
