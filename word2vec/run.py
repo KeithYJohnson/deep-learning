f44dd87818e8cdffce53b54b83d9057c4dc105ea
Keith Johnson <keithyjohnson@gmail.com>
2016-12-13 15:48:54 -0500

    Downloading word data to zip

---
+++ word2vec/run.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+import math
+import numpy as np
+import random
+import tensorflow as tf
+from matplotlib import pylab
+from six.moves import range
+from sklearn.manifold import TSNE
+filename = 'text8.zip'
